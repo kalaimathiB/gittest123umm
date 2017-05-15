a09102478589cbc3adab6868579aee6fb08f809a
kalaimathiB <kalaimathi353@gmail.com>
2017-05-15 12:36:33 +0530

    added new

---
+++ gittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by UMMADC-MAC02 on 15/05/17.
+//  Copyright © 2017 UMMADC-MAC02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
