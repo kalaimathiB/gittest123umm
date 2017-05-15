daf1f7090f9bcfdb260e6a48c9b26d864a38015e
kalaimathiB <kalaimathi353@gmail.com>
2017-05-15 14:17:42 +0530

    changs committed

--- gittest/ViewController.swift
+++ gittest/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 UMMADC-MAC02. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,6 +14,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("aded new test1")
+        print("aded new test2")
+        print("aded new test3")
+
     }
 
     override func didReceiveMemoryWarning() {
