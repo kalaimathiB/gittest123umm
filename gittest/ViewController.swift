0918e4769df2db0c3d0da3a6223f68d863c5119e
kalaimathiB <kalaimathi353@gmail.com>
2017-05-15 14:32:27 +0530

    cc

--- gittest/ViewController.swift
+++ gittest/ViewController.swift
@@ -9,19 +9,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
-        print("aded new test1")
-        print("aded new test2")
-        print("aded new test3")
-
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
